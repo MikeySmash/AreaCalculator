6c9d5b8918fc35fd350601762fbbab5c0f9eaf44
Mike Smith <mijuamha@gmail.com>
2019-08-28 09:01:17 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Area = " + AreaCalculator.area(2));
+        System.out.println("Area = " + AreaCalculator.area(2, 4));
+    }
+
+}
